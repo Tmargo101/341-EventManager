4d0f386a3a5ad7491324ed0156d2b62273f8a89b
Thomas Margosian <tmargo101@gmail.com>
2020-02-28 11:35:32 -0500

    Commented Table.class.php.

--- libraries/Table.class.php
+++ libraries/Table.class.php
@@ -2,23 +2,31 @@
 
 class Table {
 
+    // Called from HTMLElements::CreateDiv, which is called from the view (admin.php, events.php, registration.php)
     public static function createTable($controller, $getSomething) {
         $table = "";
+
+        // Uses the passed userLevel controller to get the requested data.
         $data = $controller::$getSomething();
+
+        // If data is returned, create the table using the returned data array (objects or arrays).
         if ($data != null) {
             $table .= Table::createHeader($data[0]);
             for ($i = 0; $i < count($data); $i++) {
-//                    $tableDiv .= "<h3>" . $data[$i]->getName() . "</h3>";
                 $table .= Table::createRow($data[$i]);
             }
             $table .= Table::end();
         }
+
+        // Returns the full table HTML string for display at the view
         return $table;
 
     }
 
     public static function createHeader($data) {
         $tableHeader = "<div class='pb-2'><table class='table table-striped'>\n<thead class='thead-dark'><tr>";
+
+        // Creates the appropriate Table header based on the first returned object's getType method (String which is in all classes in the model).
         switch ($data->getType()) {
             case "Attendee":
                 $tableHeader .= "
@@ -27,6 +35,7 @@ class Table {
                     <th>Role</th>
                     <th style='width: 15%'></th>";
                 break;
+
             case "Venue":
                 $tableHeader .= "
                     <th>ID</th>
@@ -34,6 +43,7 @@ class Table {
                     <th>Max Capacity</th>
                     <th  style='width: 15%'></th>";
                 break;
+
             case "Event":
                 $tableHeader .= "
                     <th>ID</th>
@@ -44,6 +54,7 @@ class Table {
                     <th>Max Capacity</th>
                     <th style='width: 15%'></th>";
                 break;
+
             case "Session":
                 $tableHeader .= "
                     <th>ID</th>
@@ -54,17 +65,23 @@ class Table {
                     <th>Venue</th>
                     <th style='width: 15%'></th>";
                 break;
+
             default:
                 $tableHeader .= "";
         }
+
+        // If the user is authenticated as an admin, draw the Add button.
         if ($_SESSION['auth']['role'] == "admin") {
             $tableHeader .= "<div class='pull-right my-2'><form action='crud.php' method='get'><button class='btn btn-outline-success' name='add' value='{$data->getType()}'>Add {$data->getType()}</button></form></div>";
         }
         $tableHeader .= "</tr></thead>\n";
+
+        // Returns the header as a string.
         return $tableHeader;
     }
 
     public static function createRow($data) {
+        // Creates the appropriate Table row based on the passed object's ($data) getType method (String which is in all classes in the model).
         switch ($data->getType()) {
             case "Attendee":
                 $row = "<tr>
@@ -72,21 +89,26 @@ class Table {
                     <td>" . $data->getName() . "</td>
                     <td>" . $data->getRole() . "</td>
                     ";
+
+                // Draw the appropriate controls, based on the user level.
+                // If the user is an admin and the attendee name is NOT admin, draw the edit and delete buttons (cannot edit or delete admin).
                 if ($_SESSION['auth']['role'] == 'admin' && $data->getName() != "admin") {
                     $row .= "<td><form action='crud.php' method='get'><button class='btn btn-primary mx-2' name='edit{$data->getType()}' value='{$data->getIdattendee()}'>Edit</button><button class='btn btn-danger' name='delete{$data->getType()}' value='{$data->getIdattendee()}'>Delete</button></form></td>";
-                } else if ($data->getName() != "admin") {
-                    $row .= "<td><form action='crud.php' method='get'><button class='btn btn-primary mx-2' name='register{$data->getType()}' value='{$data->getIdattendee()}'>Register</button></form></td>";
                 } else {
                     $row .= "<td></td>";
                 }
                 $row .= "</tr>";
                 break;
+
             case "Venue":
                 $row = "<tr>
                     <td>" . $data->getIdvenue() . "</td>
                     <td>" . $data->getName() . "</td>
                     <td>" . $data->getCapacity() . "</td>
                     ";
+
+                // Draw the appropriate controls, based on the user level.
+                // If the user is an admin and is currently viewing the attendee page, draw the edit and delete buttons (cannot edit or delete on non-admin pages).
                 if ($_SESSION['auth']['role'] == 'admin' && $_SERVER['REQUEST_URI'] == '/~txm5483/341/project1/admin.php') {
                     $row .= "<td><form action='crud.php' method='get'><button class='btn btn-primary mx-2' name='edit{$data->getType()}' value='{$data->getIdvenue()}'>Edit</button><button class='btn btn-danger' name='delete{$data->getType()}' value='{$data->getIdvenue()}'>Delete</button></form></td>";
                 } else if ($data->getName() != "admin") {
@@ -95,6 +117,7 @@ class Table {
                     $row .= "<td></td>";
                 }
                 break;
+
             case "Event":
                 $row = "<tr>
                     <td>" . $data->getIdevent() . "</td>
@@ -104,14 +127,19 @@ class Table {
                     <td>" . substr($data->getDateend(), 0, 10) . "</td>
                     <td>" . $data->getNumberallowed() . "</td>
                     ";
+
+                // Draw the appropriate controls, based on the user level.
+                // If the user is an admin and is currently viewing the attendee page, draw the edit and delete buttons (cannot edit or delete on non-admin pages).
                 if ($_SESSION['auth']['role'] == 'admin' && $_SERVER['REQUEST_URI'] == '/~txm5483/341/project1/admin.php') {
                     $row .= "<td><form action='crud.php' method='get'><button class='btn btn-primary mx-2' name='edit{$data->getType()}' value='{$data->getIdevent()}'>Edit</button><button class='btn btn-danger' name='delete{$data->getType()}' value='{$data->getIdevent()}'>Delete</button></form></td>";
+                    // TODO: change this elseif from (if dataname is not admin) to (if you can register)
                 } else if ($data->getName() != "admin") {
                     $row .= "<td><form action='crud.php' method='get'><button class='btn btn-primary mx-2' name='register{$data->getType()}' value='{$data->getIdevent()}'>Register</button></form></td>";
                 } else {
                     $row .= "<td></td>";
                 }
                 break;
+
             case "Session":
                 $row = "<tr>
                     <td>" . $data->getIdsession() . "</td>
@@ -121,6 +149,8 @@ class Table {
                     <td>" . $data->getNumberallowed() . "</td>
                     <td>" . $data->getEvent() . "</td>
                     ";
+
+                // Draw the appropriate controls, based on the user level.
                 if ($_SESSION['auth']['role'] == 'admin') {
                     $row .= "<td><form action='crud.php' method='get'><button class='btn btn-primary mx-2' name='edit{$data->getType()}' value='{$data->getIdsession()}'>Edit</button><button class='btn btn-danger'  name='delete{$data->getType()}' value='{$data->getIdsession()}'>Delete</button></form></td>";
                 } else if ($data->getName() != "admin") {
@@ -129,6 +159,7 @@ class Table {
                     $row .= "<td></td>";
                 }
                 break;
+
             default:
                 $row = "";
         }
